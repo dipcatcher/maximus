173a7931aeed66e852668e8ff7b930136d6a7db3
dipcatcher <98191759+dipcatcher@users.noreply.github.com>
2022-03-29 09:16:23 -0700

    Update maximus.sol

--- maximus.sol
+++ maximus.sol
@@ -7,19 +7,12 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-
-/// @title HEX Contract Proxy
-/// @author Tanto Nomini
-/// @dev HEX Contract Proxy
-
 contract HedronToken {
-  
   function approve(address spender, uint256 amount) external returns (bool) {}
   function transfer(address recipient, uint256 amount) external returns (bool) {}
-  
   function mintNative(uint256 stakeIndex, uint40 stakeId) external returns (uint256) {}
   function claimNative(uint256 stakeIndex, uint40 stakeId) external returns (uint256) {}
- 
+  function currentDay() external view returns (uint256) {}
 }
 
 contract HEXToken {
@@ -34,50 +27,42 @@ contract MyToken is ERC20, ERC20Burnable, Ownable {
     // all days are measured in terms of the HEX contract day number
     uint256 MINTING_PHASE_START;
     uint256 MINTING_PHASE_END;
-    bool HAS_STAKE_STARTED;
-    bool HAS_STAKE_ENDED;
     uint256 STAKE_START_DAY;
     uint256 STAKE_END_DAY;
     uint256 REDEMPTION_RATE; 
     uint256 HEDRON_REDEMPTION_RATE;
+    bool HAS_STAKE_STARTED;
+    bool HAS_STAKE_ENDED;
     bool HAS_HEDRON_MINTED;
     uint256 STAKE_LENGTH;
     uint256 STAKE_ID;
     
-    constructor(uint256 start_day, uint256 mint_duration, uint256 stake_duration) ERC20("Maximus hedron test", "MAXI") {
-        
+    constructor(uint256 mint_duration, uint256 stake_duration) ERC20("Maximus", "MAXI") {
+        uint256 start_day=hex_token.currentDay();
         MINTING_PHASE_START = start_day;
         MINTING_PHASE_END = start_day+mint_duration;
+        STAKE_LENGTH=stake_duration; 
         HAS_STAKE_STARTED=false;
         HAS_STAKE_ENDED = false;
         HAS_HEDRON_MINTED=false;
         REDEMPTION_RATE=100000000; // HEX and MAXI are 1:1 convertible up until the stake is initiated
-        HEDRON_REDEMPTION_RATE=100000000;
-        STAKE_LENGTH=stake_duration; 
+        HEDRON_REDEMPTION_RATE=0; //no hedron is redeemable until minting has occurred
+        renounceOwnership();
     }
     
     /**
-    * @dev View number of decimal places the MAXI token is divisible to. Manually set to 8 to match that of HEX. 1 MAXI = 10^8 mini
-    
+    * @dev View number of decimal places the MAXI token is divisible to. Manually overwritten from default 18 to 8 to match that of HEX. 1 MAXI = 10^8 mini
     */
     function decimals() public view override returns (uint8) {
         return 8;
 	}
-    address CONTRACT_ADDRESS =address(this);
+    address MAXI_ADDRESS =address(this);
     address HEX_ADDRESS = 0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39; // "2b, 5 9 1e? that is the question..."
-    /*
-    HDRN Token Contract Addresses:
+    address HEDRON_ADDRESS=0x3819f64f282bf135d62168C1e513280dAF905e06; // PULSECHAIN TESTNET HEDRON ADDRESS
 
-    Pulse Testnet V2
-    0xDC11f7E700A4c898AE5CAddB1082cFfa76512aDD
-
-    Ethereum
-    0x3819f64f282bf135d62168C1e513280dAF905e06
-    */
-    address HEDRON_ADDRESS=0xDC11f7E700A4c898AE5CAddB1082cFfa76512aDD;
     IERC20 hex_contract = IERC20(HEX_ADDRESS);
     IERC20 hedron_contract=IERC20(HEDRON_ADDRESS);
-    HEXToken token = HEXToken(HEX_ADDRESS);
+    HEXToken hex_token = HEXToken(HEX_ADDRESS);
     HedronToken hedron_token = HedronToken(HEDRON_ADDRESS);
     // public function
     /**
@@ -116,13 +101,30 @@ contract MyToken is ERC20, ERC20Burnable, Ownable {
     * @return Current HEX Day
     */
     function getHexDay() public view returns (uint256){
-        uint256 day = token.currentDay();
+        uint256 day = hex_token.currentDay();
         return day;
     }
+     /**
+    * @dev Returns the current HEDRON day."
+    * @return day Current HEDRON Day
+    */
+    function getHedronDay() public view returns (uint day) {return hedron_token.currentDay();}
 
+    /**
+     * @dev Used to calculate a ratio considering decimal rounding.
+     * @param numerator HEX balance of contract address after stake ends
+     * @param denominator Total MAXI supply
+     * @param precision number of decimals to cut off at.
+     * @return quotient 
+     */
+    function percent(uint numerator, uint denominator, uint precision) private view returns(uint quotient) {
+        uint _numerator  = numerator * 10 ** (precision+1);
+        //rounding of last digit
+        uint _quotient =  ((_numerator / denominator) + 5) / 10;
+        return ( _quotient);
+  }
 
-    // MAXI Minting Functions
-
+    // MAXI Issuance and Redemption Functions
     /**
      * @dev Mints MAXI.
      * @param amount of MAXI to mint, measured in minis
@@ -132,20 +134,17 @@ contract MyToken is ERC20, ERC20Burnable, Ownable {
     }
      /**
      * @dev Ensures that MAXI Minting Phase is ongoing and that the user has allowed the Maximus Contract address to spend the amount of HEX the user intends to pledge to Maximus DAO. Then sends the designated HEX from the user to the Maximus Contract address and mints 1 MAXI per HEX pledged.
-
      * @param amount of HEX user chose to pledge, measured in hearts
      */
     function pledgeHEX(uint256 amount) public {
-        require(HEXToken(HEX_ADDRESS).currentDay()<=MINTING_PHASE_END, "Minting Phase is Done");
-        require(hex_contract.allowance(msg.sender, CONTRACT_ADDRESS)>=amount, "Please approve contract address as allowed spender in the hex contract.");
+        require(hex_token.currentDay()<=MINTING_PHASE_END, "Minting Phase is Done");
+        require(hex_contract.allowance(msg.sender, MAXI_ADDRESS)>=amount, "Please approve contract address as allowed spender in the hex contract.");
         address from = msg.sender;
-        hex_contract.transferFrom(from, CONTRACT_ADDRESS, amount);
+        hex_contract.transferFrom(from, MAXI_ADDRESS, amount);
         mint(amount);
     }
-
      /**
      * @dev Ensures that it is currently a redemption period and that the user has at least the number of maxi they entered. Then it calculates how much hex may be redeemed, burns the MAXI, and transfers them the hex..
-
      * @param amount_MAXI number of MAXI that the user is redeeming, measured in mini
      */
     function redeemHEX(uint256 amount_MAXI) public {
@@ -154,17 +153,13 @@ contract MyToken is ERC20, ERC20Burnable, Ownable {
         require(yourMAXI>=amount_MAXI, "You do not have that much MAXI.");
         uint256 raw_redeemable_amount = amount_MAXI*REDEMPTION_RATE;
         uint256 redeemable_amount = raw_redeemable_amount/100000000;
-        uint256 raw_redeemable_hedron = amount_MAXI*HEDRON_REDEMPTION_RATE;
-        uint256 redeemable_hedron = raw_redeemable_hedron/100000000;
-
-
         burn(amount_MAXI);
-        token.transfer(msg.sender, redeemable_amount);
+        hex_token.transfer(msg.sender, redeemable_amount);
         if (HAS_HEDRON_MINTED==true) {
+            uint256 raw_redeemable_hedron = amount_MAXI*HEDRON_REDEMPTION_RATE;
+            uint256 redeemable_hedron = raw_redeemable_hedron/100000000;
             hedron_token.transfer(msg.sender, redeemable_hedron);
-
         }
-        
     }
     //Staking Functions
     // Anyone may run these functions during the allowed time, so long as they pay the gas.
@@ -175,7 +170,7 @@ contract MyToken is ERC20, ERC20Burnable, Ownable {
      */
     function stakeHEX() public {
         require(HAS_STAKE_STARTED==false, "Stake has already been started.");
-        uint256 current_day = HEXToken(HEX_ADDRESS).currentDay();
+        uint256 current_day = hex_token.currentDay();
         require(current_day>MINTING_PHASE_END, "Minting Phase is still ongoing - see MINTING_PHASE_END day.");
         uint256 amount = hex_contract.balanceOf(address(this)); //contract stakes all hex in the contract. even hex acccidentally sent to the contract without minting.
         _stakeHEX(amount);
@@ -184,11 +179,11 @@ contract MyToken is ERC20, ERC20Burnable, Ownable {
         STAKE_END_DAY=current_day+STAKE_LENGTH;
     }
     function _stakeHEX(uint256 amount) private  {
-        token.stakeStart(amount,STAKE_LENGTH);
+        hex_token.stakeStart(amount,STAKE_LENGTH);
         }
     
     function _endStakeHEX(uint256 stakeIndex,uint40 stakeIdParam ) private  {
-        token.stakeEnd(stakeIndex, stakeIdParam);
+        hex_token.stakeEnd(stakeIndex, stakeIdParam);
         }
     /**
      * @dev Ensures that the stake is fully complete and that it has not already been ended. Then it ends the hex stake and updates the redemption rate.
@@ -196,7 +191,7 @@ contract MyToken is ERC20, ERC20Burnable, Ownable {
      * @param stakeIdParam stake identifier found in stakeLists[contract_address] in hex contract.
      */
     function endStakeHEX(uint256 stakeIndex,uint40 stakeIdParam ) public {
-        require(HEXToken(HEX_ADDRESS).currentDay()>STAKE_END_DAY, "Stake is not complete yet.");
+        require(hex_token.currentDay()>STAKE_END_DAY, "Stake is not complete yet.");
         require(HAS_STAKE_STARTED==true && HAS_STAKE_ENDED==false, "Stake has already been started.");
         _endStakeHEX(stakeIndex, stakeIdParam);
         HAS_STAKE_ENDED=true;
@@ -213,37 +208,18 @@ contract MyToken is ERC20, ERC20Burnable, Ownable {
         return RR;
     }
     /**
-     * @dev Used to calculate a ratio considering decimal rounding.
-     * @param numerator HEX balance of contract address after stake ends
-     * @param denominator Total MAXI supply
-     * @param precision number of decimals to cut off at.
-     * @return quotient 
+     * @dev Public function which calls the private function which is used for minting available HDRN accumulated by the contract stake. 
+     * @param stakeIndex index of stake found in stakeLists[contract_address] in hex contract.
+     * @param stakeId stake identifier found in stakeLists[contract_address] in hex contract.
      */
-    function percent(uint numerator, uint denominator, uint precision) public view returns(uint quotient) {
-        // helper for calculating percent
-         // caution, check safe-to-multiply here
-        uint _numerator  = numerator * 10 ** (precision+1);
-        // with rounding of last digit
-        uint _quotient =  ((_numerator / denominator) + 5) / 10;
-        return ( _quotient);
-
-    
-  }
-  function claimHedron(uint256 stakeIndex,uint40 stakeId ) public  {
-      
-      _claimHedron(stakeIndex, stakeId);
-      
-        }
-  function _claimHedron(uint256 stakeIndex,uint40 stakeId ) private  {
-        hedron_token.claimNative(stakeIndex, stakeId);
-        
-        }
-
   function mintHedron(uint256 stakeIndex,uint40 stakeId ) public  {
-      
       _mintHedron(stakeIndex, stakeId);
-      
         }
+   /**
+     * @dev Private function used for minting available HDRN accumulated by the contract stake and updating the HDRON redemption rate.
+     * @param stakeIndex index of stake found in stakeLists[contract_address] in hex contract.
+     * @param stakeId stake identifier found in stakeLists[contract_address] in hex contract.
+     */
   function _mintHedron(uint256 stakeIndex,uint40 stakeId ) private  {
         hedron_token.mintNative(stakeIndex, stakeId);
         uint256 total_hedron= hedron_contract.balanceOf(address(this));
@@ -251,5 +227,4 @@ contract MyToken is ERC20, ERC20Burnable, Ownable {
         HEDRON_REDEMPTION_RATE = percent(total_hedron, total_maxi, 8);
         HAS_HEDRON_MINTED = true;
         }
-    
 }
